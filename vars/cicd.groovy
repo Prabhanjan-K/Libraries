29a1ddfbf82bb7364fb6da714b3d012c17a6f4a0
Prabhanjan-K <kprabhanjan9@gmail.com>
2024-10-03 16:09:00 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,4 +12,11 @@ def deploytomcat(jobname,ip,context)
 {
    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }  
-   
+ def testing(jobname)
+{
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+def delivery()
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}  
